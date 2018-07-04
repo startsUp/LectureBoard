bd43c79427b4683b359fe1115ba2f041b55a461e
shardool <pates25@mcmaster.ca>
2018-07-04 14:44:10 -0400

    storing paths in an array

--- client/src/components/svgDrawBoard/drawBoard.js
+++ client/src/components/svgDrawBoard/drawBoard.js
@@ -1,87 +1,126 @@
 import React, { Component } from 'react';
 import './drawBoard.css';
 
-function getCursorPosition(svgBoard, event) {
+var ws;
+
+function getCursorPosition(event) {
+    var svgBoard = document.getElementById('mainBoard');
     var rectB = svgBoard.getBoundingClientRect();
     var cx = event.clientX - rectB.left;
     var cy = event.clientY - rectB.top;
     return  [cx, cy];
 }
 
-
-
-var ws;
+function getPath(path){
+    return(
+        <path d={path} className='path' zoomAndPan='magnify'/>
+    );
+}
 class DrawBoard extends Component {
     constructor(props)
     {
-        ws = new WebSocket('ws://localhost:40510');
-        ws.onopen = function () {
-        console.log('websocket is connected ...');
-        ws.send('connected');
-        }
-
-        ws.onmessage = function (ev) {
-        console.log('message recieved');
-        }
+        
         super(props);
         this.state = {
             strokes : [],
-            drawing : false,
+            strokeCount: 0,
+            drawing : false, 
             strokeWidth : 0.5,
             path : "",
-            points: [],
             lastPoint : [0, 0],
             socket: ws
         };
+        
+        
+        
 
+        this.updateState = this.updateState.bind(this);
         this.finishDraw = this.finishDraw.bind(this);
         this.draw = this.draw.bind(this);
         this.startDraw = this.startDraw.bind(this);
+        this.startNewStroke = this.startNewStroke.bind(this);
+        this.addToStroke = this.addToStroke.bind(this);
+        this.finishStroke= this.finishStroke.bind(this);
 
-    }
 
+        ws = new WebSocket('ws://localhost:40510');
+        ws.onopen = () => {
+            console.log('websocket is connected ...');
+        }
 
-
-    startDraw(e){
-        const pStrokes = this.state.strokes;
-        const cStrokeWidth = this.state.strokeWidth;
-        let cPath = this.state.path;
-        const svgBoard = document.getElementById('mainBoard');
-        let coords = getCursorPosition(svgBoard, e);
-        let nPath = cPath + "M"+ coords[0] + "," + coords[1] + " ";
+        ws.onmessage = (message) => {
+            var flag = message.data[0];
         
+            if(flag==='M')
+            {
+                this.setState({path:message.data});
+            }
+            else
+            {
+                const cPath = this.state.path;
+                this.setState({path:cPath+message.data});
+            }
+            console.log(flag); 
+        }
+    }
+
+    startNewStroke(nPath)
+    {
+        this.setState({path:nPath});
+    }
+    addToStroke(nPath)
+    {
+        
+    }
+    finishStroke(){}
+
+    updateState(coords, flag){
+        let nPath,
+            drawingStatus = true;
+        let cPath = this.state.path;
+
+        if(flag==='i')
+            nPath = "M"+ coords[0] + "," + coords[1] + " ";
+        else
+            nPath = "L"+ coords[0] + "," + coords[1] + " ";
+        const nStrokes = this.state.strokes;
+        const updatedPath = cPath + nPath;
+        
+        if(!nStrokes)
+        {
+            nStrokes.push(updatedPath);
+        }
+        else
+        {
+            nStrokes[this.state.strokeCount] = updatedPath; 
+        }
+
         this.setState({
-            strokes:pStrokes,
-            drawing: true,
-            strokeWidth: cStrokeWidth,
-            path: nPath,
-            lastPoint: coords
+            strokes: nStrokes,
+            drawing: drawingStatus,
+            path: updatedPath,
+            lastPoint: coords,
          });
-        ws.send(nPath);
+        
+        if(ws.readyState === WebSocket.OPEN)
+            ws.send(nPath);
     }
 
 
-    draw(e){
+    startDraw(e)
+    {
+        let coords = getCursorPosition(e);
+        this.updateState(coords, 'i');
+    }
+
+
+    draw(e)
+    {
         const isDrawing = this.state.drawing;
         if(isDrawing)
         {
-            const pStrokes = this.state.strokes;
-            const cStrokeWidth = this.state.strokeWidth;
-            let cPath = this.state.path;
-            const svgBoard = document.getElementById('mainBoard');
-            let coords = getCursorPosition(svgBoard, e);
-            let nPath = cPath + "L"+ coords[0] + "," + coords[1] + " ";
-
-
-            this.setState({
-                strokes:pStrokes,
-                drawing: true,
-                strokeWidth: cStrokeWidth,
-                path: nPath,
-                lastPoint: coords
-
-             });
-
+            let coords = getCursorPosition(e);
+            this.updateState(coords, 'd');
         }
 
 
@@ -91,21 +130,27 @@ class DrawBoard extends Component {
         const isDrawing = this.state.drawing;
         if(isDrawing)
         {
-            const pStrokes = this.state.strokes;
-            const cStrokeWidth = this.state.strokeWidth;
-            let path = this.state.path;
-            console.log(path);
-            this.setState({strokes:pStrokes, drawing: false, strokeWidth: cStrokeWidth});
+            this.setState({
+                drawing: false,
+                strokeCount : this.state.strokeCount+1,
+            });
         }
 
     }
 
     render(){
 
+        const strokes = this.state.strokes.map((stroke) => {
+           // console.log(stroke);
+            return(
+                <path d={stroke} className='path' zoomAndPan='magnify'/>
+            );
+        });
+        //console.log(strokes);
 
-         return(
+        return(
           <svg className='board' id='mainBoard' zoomAndPan='magnify' onMouseDown={this.startDraw} onMouseMove={this.draw} onMouseUp={this.finishDraw} onMouseLeave={this.finishDraw}>
-          <path d={this.state.path} className='path' zoomAndPan='magnify' />
+          {strokes}
           </svg>
 
         );
--- ws.js
+++ ws.js
@@ -1,19 +1,18 @@
 var WebSocket = require('ws');
 var wss = new WebSocket.Server({port: 40510});
+var clientCount = 0;
+//temp solution: store client ids in an array
+var clients = []; 
 
-wss.broadcast = (data) => {
-	wss.clients.forEach((client) => {
-		if (client.readyState === WebSocket.OPEN) {
-     		client.send(data);
-    	}
-	});
-}
 wss.on('connection',  (ws) => {
+  console.log('connected client:' + clientCount);
   ws.on('message', (message) => {
     wss.clients.forEach((client) => {
-    	if(client !== ws && client.readyState === WebSocket.OPEN)
-    		client.send(message);
+      if(client !== ws && client.readyState === WebSocket.OPEN)
+        client.send(message);
     });
   });
-
 });
+
+
+
